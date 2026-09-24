32b626d17b94da74e6c54e922643b8172dfa71ea
Késsia Silva <kessiasilvadealmeida@gmail.com>
2026-09-24 09:20:26 -0300

    feat: final da aula d K-Means

---
+++ GerenciamentoImagens/Aula 24-09-2026/K-Means.py
@@ -0,0 +1,137 @@
+import time
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+img = cv2.imread('GatoLuna.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+n, m, c = img.shape
+print(f'linhas: {n} colunas: {m} canais: {c}')
+
+# Parte de inicialização:
+faixa = []
+plano = []
+
+for cor in range(c):
+    plano.append(img[:, :, cor])
+    faixa.append(plano[-1].flatten())
+    plt.scatter(np.arange(len(faixa[-1])), faixa[-1])
+
+K = 2
+centroid = []
+n_pixels = len(faixa[0])
+
+for k in range(K):
+    px = np.random.randint(0, n_pixels)
+
+    v = []
+
+    for f in faixa:
+        v.append(f[px])
+
+    centroid.append(np.array(v))
+    print(centroid[-1])
+
+
+MAX_ITER = 200
+iter = 0
+while True:
+    CLUSTER = [[] for _ in range(K)]
+
+    # Parte de Agrupamento:
+    tic = time.time()
+
+    for px in range(n_pixels):
+
+        v = [
+            faixa[0][px],
+            faixa[1][px],
+            faixa[2][px]
+        ]
+
+        d = []
+
+        for k in range(K):
+            d.append(
+                np.linalg.norm(
+                    np.array(v) - np.array(centroid[k])
+                )
+            )
+
+        cluster = np.argmin(d)
+        CLUSTER[cluster].append(px)
+
+    print(len(CLUSTER[0]), len(CLUSTER[1]))
+    print(f'(temp: {time.time() - tic})S')
+
+
+    # Parte do Centroid:
+    novo_centroid = []
+
+    for k in range(K):
+
+        L = len(CLUSTER[k])
+        S = np.zeros(3)
+
+        for px in CLUSTER[k]:
+            S += [
+                faixa[0][px],
+                faixa[1][px],
+                faixa[2][px]
+            ]
+
+        novo_centroid.append([
+            int(S[0] / L),
+            int(S[1] / L),
+            int(S[2] / L)
+        ])
+
+
+    # Verificação
+    conv = True
+
+    for k in range(K):
+
+        for c in range(3):
+
+            if centroid[k][c] != novo_centroid[k][c]:
+                conv = False
+                break
+
+
+    print(f'Convergiu: {conv}')
+
+
+    # Controle das iterações
+    if not conv:
+
+        centroid = novo_centroid
+        iter += 1
+        print(f'Iterações {iter}')
+
+        if iter >= MAX_ITER:
+            print(f'não convergiram em {MAX_ITER} iterações')
+            break
+
+    else:
+
+        print(f'Convergiu em {iter} iterações')
+        break
+
+H, W, C = img.shape
+color = [[255,0,0], [0,255,0], [0,0,255]]
+segmented = np.empty((H, W, 3), dtype=np.uint8)
+for k in range(K):
+    for px in CLUSTER[k]:
+        i = px//W
+        j = px % W
+        segmented[i,j] = color[k]
+plt.imshow(segmented)
+plt.show()
+
+L =0
+for k in range(K):
+    L+= len(CLUSTER[k])
+print(f' L ={L} vs {H*W}')
+
